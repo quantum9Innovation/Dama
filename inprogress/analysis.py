6ad352b1e6521e52c1f2d8dcb5b065ed31c2d729
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2020-10-18 20:43:51 -0700

    Changed to a formatting agent

--- inprogress/analysis.py
+++ inprogress/analysis.py
@@ -3,7 +3,7 @@ import numpy as np
 from copy import deepcopy
 
 
-def model_out(board):
+def formatter(board):
 
     raw_fen = board.fen().split(' ')
     fen = raw_fen[0].split('/')
@@ -11,20 +11,20 @@ def model_out(board):
     turn = raw_fen[1]
     castling_info = list(raw_fen[2])
 
-    castle_k = False
-    castle_K = False
-    castle_q = False
-    castle_Q = False
+    castle_k = 13
+    castle_K = 14
+    castle_q = 15
+    castle_Q = 16
 
     for char in castling_info:
         if char == 'k':
-            castle_k = True
+            castle_k = 17
         if char == 'K':
-            castle_K = True
+            castle_K = 18
         if char == 'q':
-            castle_q = True
+            castle_q = 19
         if char == 'Q':
-            castle_Q = True
+            castle_Q = 20
 
     net_input = []
     piece_mobility = [0] * 64
@@ -71,49 +71,93 @@ def model_out(board):
         line_bonus = 0
         for char in range(0, len(fen_string)):
 
-            if fen_string[char] == 'P':
-                net_input.append(1)
-                wpco.append([char + line_bonus, line])
-            elif fen_string[char] == 'p':
-                net_input.append(2)
-                bpco.append([char + line_bonus, line])
-            elif fen_string[char] == 'R':
-                net_input.append(3)
-                wrco.append([char + line_bonus, line])
-            elif fen_string[char] == 'r':
-                net_input.append(4)
-                brco.append([char + line_bonus, line])
-            elif fen_string[char] == 'N':
-                net_input.append(5)
-                wnco.append([char + line_bonus, line])
-            elif fen_string[char] == 'n':
-                net_input.append(6)
-                bnco.append([char + line_bonus, line])
-            elif fen_string[char] == 'B':
-                net_input.append(7)
-                wbco.append([char + line_bonus, line])
-            elif fen_string[char] == 'b':
-                net_input.append(8)
-                bbco.append([char + line_bonus, line])
-            elif fen_string[char] == 'K':
-                net_input.append(9)
-                wkco.append([char + line_bonus, line])
-            elif fen_string[char] == 'k':
-                net_input.append(10)
-                bkco.append([char + line_bonus, line])
-            elif fen_string[char] == 'Q':
-                net_input.append(11)
-                wqco.append([char + line_bonus, line])
-            elif fen_string[char] == 'q':
-                net_input.append(12)
-                bqco.append([char + line_bonus, line])
-            else:
-                num_squares = int(fen_string[char])
-                line_bonus += num_squares - 1
-                for i in range(num_squares):
-                    net_input.append(0)
+            if turn == 'w':
 
-    net_input = np.array(net_input)
+                if fen_string[char] == 'P':
+                    net_input.append(1)
+                    wpco.append([char + line_bonus, line])
+                elif fen_string[char] == 'p':
+                    net_input.append(2)
+                    bpco.append([char + line_bonus, line])
+                elif fen_string[char] == 'R':
+                    net_input.append(3)
+                    wrco.append([char + line_bonus, line])
+                elif fen_string[char] == 'r':
+                    net_input.append(4)
+                    brco.append([char + line_bonus, line])
+                elif fen_string[char] == 'N':
+                    net_input.append(5)
+                    wnco.append([char + line_bonus, line])
+                elif fen_string[char] == 'n':
+                    net_input.append(6)
+                    bnco.append([char + line_bonus, line])
+                elif fen_string[char] == 'B':
+                    net_input.append(7)
+                    wbco.append([char + line_bonus, line])
+                elif fen_string[char] == 'b':
+                    net_input.append(8)
+                    bbco.append([char + line_bonus, line])
+                elif fen_string[char] == 'K':
+                    net_input.append(9)
+                    wkco.append([char + line_bonus, line])
+                elif fen_string[char] == 'k':
+                    net_input.append(10)
+                    bkco.append([char + line_bonus, line])
+                elif fen_string[char] == 'Q':
+                    net_input.append(11)
+                    wqco.append([char + line_bonus, line])
+                elif fen_string[char] == 'q':
+                    net_input.append(12)
+                    bqco.append([char + line_bonus, line])
+                else:
+                    num_squares = int(fen_string[char])
+                    line_bonus += num_squares - 1
+                    for i in range(num_squares):
+                        net_input.append(0)
+
+            else:
+
+                if fen_string[char] == 'p':
+                    net_input.append(1)
+                    wpco.append([char + line_bonus, line])
+                elif fen_string[char] == 'P':
+                    net_input.append(2)
+                    bpco.append([char + line_bonus, line])
+                elif fen_string[char] == 'r':
+                    net_input.append(3)
+                    wrco.append([char + line_bonus, line])
+                elif fen_string[char] == 'R':
+                    net_input.append(4)
+                    brco.append([char + line_bonus, line])
+                elif fen_string[char] == 'n':
+                    net_input.append(5)
+                    wnco.append([char + line_bonus, line])
+                elif fen_string[char] == 'N':
+                    net_input.append(6)
+                    bnco.append([char + line_bonus, line])
+                elif fen_string[char] == 'b':
+                    net_input.append(7)
+                    wbco.append([char + line_bonus, line])
+                elif fen_string[char] == 'B':
+                    net_input.append(8)
+                    bbco.append([char + line_bonus, line])
+                elif fen_string[char] == 'k':
+                    net_input.append(9)
+                    wkco.append([char + line_bonus, line])
+                elif fen_string[char] == 'K':
+                    net_input.append(10)
+                    bkco.append([char + line_bonus, line])
+                elif fen_string[char] == 'q':
+                    net_input.append(11)
+                    wqco.append([char + line_bonus, line])
+                elif fen_string[char] == 'Q':
+                    net_input.append(12)
+                    bqco.append([char + line_bonus, line])
+                else:
+                    num_squares = int(fen_string[char])
+                    line_bonus += num_squares - 1
+                    for i in range(num_squares):
+                        net_input.append(0)
 
     while len(wpco) < 8:
         wpco.append([])
@@ -133,11 +177,23 @@ def model_out(board):
         bbco.append([])
 
     piece_map = [wpco, bpco, wrco, brco, wnco, bnco, wbco, bbco, wkco, bkco, wqco, bqco]
+    if turn == 'b':
+        piece_map = [bpco, wpco, brco, wrco, bnco, wnco, bbco, wbco, bkco, wkco, bqco, wqco]
 
-    return [turn, [castle_K, castle_k, castle_Q, castle_q], net_input.reshape((8, 8)), piece_map,
-            piece_mobility.reshape((8, 8)), piece_threats.reshape((8, 8))]
+    castling_rights = [castle_K, castle_k, castle_Q, castle_q]
+    if turn == 'b':
+        castling_rights = [castle_k, castle_K, castle_q, castle_Q]
+
+    net_input.append(castling_rights[0])
+    net_input.append(castling_rights[1])
+    net_input.append(castling_rights[2])
+    net_input.append(castling_rights[3])
+
+    net_input = np.array(net_input)
+
+    return [net_input, piece_map, piece_mobility, piece_threats]
 
 
-board = chess.Board(fen='r2q1rk1/pb3ppp/1pnbpn2/2pp4/2PP4/P1N1PNP1/1P1B1PBP/R2QK2R w KQ - 0 1')
+board = chess.Board(fen='r2qk2r/pppb1ppp/2nbpn2/3pN3/3P1P2/1P1BPR2/PBPN2PP/R2Q2K1 b kq - 0 1')
 print(board)
-print(model_out(board))
+print(formatter(board))
